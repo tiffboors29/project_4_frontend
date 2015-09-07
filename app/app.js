cabfa683b489190432df1a74aa064e6fe535f91f
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-07 14:46:07 -0400

    Define breweriesApp and add framework for ngRoutes

---
+++ app/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+(function breweriesAppIIFE(){
+  var app = angular.module('breweriesApp', ['ngRoute']);
+
+  app.config(function($routeProvider){
+    $routeProvider
+    .when('/breweries/:state',
+    {
+      controller: 'breweriesController as brewsCtrl',
+      templateUrl: 'app/views/breweries.html'
+    }
+    )
+    .when('/breweries/:brewerydbId',
+    { // detail of brewery with list of beers
+      controller: 'breweriesContoller as brewsCtrl',
+      templateUrl: 'app/views/breweries.html'
+    }
+    )
+    .otherwise({redirectTo: '/'});
+  });
+})();
