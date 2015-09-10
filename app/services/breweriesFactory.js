88816077243efa857243b573a105b604dc525c58
tiffboors29 <tiffanyboorsma@gmail.com>
2015-09-10 13:52:23 -0400

    Add routes for getting breweries by city and state

--- app/services/breweriesFactory.js
+++ app/services/breweriesFactory.js
@@ -7,13 +7,22 @@
 
       var breweriesAPI = {};
 
-      breweriesAPI.getBreweries = function(state){
+      breweriesAPI.getStateBreweries = function(state){
         // allow access to list of breweries at location
-        return $http.get('http://localhost:3000/'+ state);
+        return $http.get('http://localhost:3000/brewerydb/state/'+ state);
+      };
+
+      breweriesAPI.getCityBreweries = function(city){
+        // allow access to list of breweries at location
+        return $http.get('http://localhost:3000/brewerydb/city/'+ city);
       };
 
       breweriesAPI.getBrewery = function(brewerydbId){
         return $http.get('http://localhost:3000/'+ brewerydbId);
+      };
+
+      breweriesAPI.getStates = function(){
+        return $http.get('http://localhost:3000/states');
       };
 
       return breweriesAPI;
